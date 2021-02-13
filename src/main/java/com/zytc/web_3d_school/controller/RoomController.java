2b09c937ea3a8bd1eb90b113f61531d93993b584
Tang-phy <1258085989@qq.com>
2021-02-13 19:10:49 +0800

    1.1把mtl改动了（你们在推送里面下载）  1.2然后我把static的十多个jpg文件放到了school里面（这个你们自己移动就行，我没有放在推送里面） 2.我把room1.html名字改成了room.html   这里面我把加载教室的函数注释了，然后实现了播放屏幕的建立与视频的加载、播放标签、暂停标签。 3.推送里面我新建了一个video文件，里面有一个视频，可以用room.html进行播放

--- src/main/java/com/zytc/web_3d_school/controller/RoomController.java
+++ src/main/java/com/zytc/web_3d_school/controller/RoomController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class RoomController {
     @RequestMapping("/room")
     public String room(){
-        return "room1";
+        return "room";
     }
 
 }
